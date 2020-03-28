0cb635ac4dd78a3b8695597b1e8b0a6b3bafd321
tienph <tienph@rikkeisoft.com>
2020-03-28 10:08:35 +0700

    spring boot

--- spring-eureka-common/src/main/java/com/eureka/common/security/JwtConfig.java
+++ spring-eureka-common/src/main/java/com/eureka/common/security/JwtConfig.java
@@ -6,21 +6,22 @@ import lombok.Getter;
 import lombok.ToString;
 
 // To use this class outside. You have to 
-	// 1. Define it as a bean, either by adding @Component or use @Bean to instantiate an object from it
-	// 2. Use the @Autowire to ask spring to auto create it for you, and inject all the values.
+// 1. Define it as a bean, either by adding @Component or use @Bean to instantiate an object from it
+// 2. Use the @Autowire to ask spring to auto create it for you, and inject all the values.
 
 // So, If you tried to create an instance manually (i.e. new JwtConfig()). This won't inject all the values. 
-	// Because you didn't ask Spring to do so (it's done by you manually!).
+// Because you didn't ask Spring to do so (it's done by you manually!).
 // Also, if, at any time, you tried to instantiate an object that's not defined as a bean
-	// Don't expect Spring will autowire the fields inside that class object.
-	
- @Getter 		// lombok will create getters auto.
- @ToString		// [IMP] You need to install lombok jar file: https://stackoverflow.com/a/11807022
+// Don't expect Spring will autowire the fields inside that class object.
+
+@Getter // lombok will create getters auto.
+@ToString // [IMP] You need to install lombok jar file:
+          // https://stackoverflow.com/a/11807022
 public class JwtConfig {
 
-	// Spring doesn't inject/autowire to "static" fields. 
-	// Link: https://stackoverflow.com/a/6897406
-	@Value("${security.jwt.uri:/auth/**}")
+    // Spring doesn't inject/autowire to "static" fields.
+    // Link: https://stackoverflow.com/a/6897406
+    @Value("${security.jwt.uri:/auth/**}")
     private String Uri;
 
     @Value("${security.jwt.header:Authorization}")
@@ -34,26 +35,26 @@ public class JwtConfig {
 
     @Value("${security.jwt.secret:JwtSecretKey}")
     private String secret;
-    
+
     // In case you want to use plain getters instead of lombok.
-	public String getUri() {
-		return Uri;
-	}
+    public String getUri() {
+        return Uri;
+    }
 
-	public String getHeader() {
-		return header;
-	}
+    public String getHeader() {
+        return header;
+    }
 
-	public String getPrefix() {
-		return prefix;
-	}
+    public String getPrefix() {
+        return prefix;
+    }
 
-	public int getExpiration() {
-		return expiration;
-	}
+    public int getExpiration() {
+        return expiration;
+    }
 
-	public String getSecret() {
-		return secret;
-	}
-    
+    public String getSecret() {
+        return secret;
+    }
+
 }
